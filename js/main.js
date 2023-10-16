793ec29a3286dbc76ae410f917ce613791f9f3e4
Илья <ilya.panarin14@gmail.com>
2023-10-16 11:53:26 +0500

    Генерация данных

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const COUNT_OBJECTS = 25;
+const MIN_PHOTO_ID = 1;
+const MAX_PHOTO_ID = 25;
+const MIN_COMMENT_ID = 1;
+const MAX_COMMENT_ID = 300;
+const MIN_AVATAR_ID = 1;
+const MAX_AVATAR_ID = 6;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_URL_ID = 1;
+const MAX_URL_ID = 25;
+
 const DESCRIPTIONS = [
   'Будь счастлив именно сейчас в текущий момент. Потому что твоя жизнь всегда состоит из текущих моментов.',
   'Будьте героями своих собственных историй!',
@@ -33,9 +45,6 @@ const NAMES = [
   'Екатерина'
 ];
 
-//Количество сгенерированных объектов
-const countObjects = 25;
-
 //Получение случайных чисел
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -61,16 +70,16 @@ const createRandomIdFromRangeGenerator = (min, max) => {
   };
 };
 
-const getPhotoId = createRandomIdFromRangeGenerator(1, 25);
-const getCommentId = createRandomIdFromRangeGenerator(1, 300);
+const getPhotoId = createRandomIdFromRangeGenerator(MIN_PHOTO_ID, MAX_PHOTO_ID);
+const getCommentId = createRandomIdFromRangeGenerator(MIN_COMMENT_ID, MAX_COMMENT_ID);
 
 //Получение случайного элемента массива
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
 
 //Массив объектов — список комментариев
 const createComments = () => ({
-  id: getCommentId(1, 300),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  id: getCommentId(),
+  avatar: `img/avatar-${getRandomInteger(MIN_AVATAR_ID, MAX_AVATAR_ID)}.svg`,
   message: `${getRandomArrayElement(MESSAGES)}`,
   name: `${getRandomArrayElement(NAMES)}`,
 });
@@ -78,13 +87,13 @@ const createComments = () => ({
 //Структура объекта
 const createObject = () => ({
   id: getPhotoId(1, 25),
-  url: `photos/${getRandomInteger(1, 25)}.jpg`,
+  url: `photos/${getRandomInteger(MIN_URL_ID, MAX_URL_ID)}.jpg`,
   description: `${getRandomArrayElement(DESCRIPTIONS)}`,
-  likes: getRandomInteger(15, 200),
-  comments: createComments(0, 30),
+  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+  comments: createComments(),
 });
 
 //Массив из 25 сгенерированных объектов
-const similarObjects = () => Array.from({length: countObjects}, createObject);
+const similarObjects = () => Array.from({length: COUNT_OBJECTS}, createObject);
 
 similarObjects();
