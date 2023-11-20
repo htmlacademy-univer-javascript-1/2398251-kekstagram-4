d0d49b155fd4e6f9de43463a663fd9048b9ed78d
Илья <ilya.panarin14@gmail.com>
2023-11-20 13:20:18 +0500

    Открывается и закрывается

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,17 +1,18 @@
+import { openBigPicture } from './bigPicture';
+
 const thumbnailTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
 const container = document.querySelector('.pictures');
 
-const createThumbnail = ({id, url, description, likes, comments}) => {
+const createThumbnail = ({url, description, likes, comments}) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
 
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__likes').textContent = likes;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
-  thumbnail.dataset.thumbnailId = id;
 
   return thumbnail;
 };
@@ -21,9 +22,12 @@ const renderTemplate = (pictures) => {
   pictures.forEach((picture) => {
     const thumbnail = createThumbnail(picture);
     fragment.appendChild(thumbnail);
-  });
+    thumbnail.addEventListener('click', () => {
+      openBigPicture(picture);
+    });
 
-  container.append(fragment);
+    container.append(fragment);
+  });
 };
 
 export {renderTemplate};
